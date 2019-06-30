bbbfb6bd3434d5c306ea26dd774743e9994f5f95
yuanzhimeng <214243830@qq.com>
2019-06-30 22:21:39 +0800

    route

--- README.md
+++ README.md
@@ -1,2 +1,131 @@
-# framework
-YzmPHP framework.
+Route
+=====
+
+### Install
+
+If you have Composer, just include route as a project dependency in your `composer.json`. If you don't just install it by downloading the .ZIP file and extracting it to your project directory.
+
+```
+require: {
+    "yzmcms/route": "master"
+}
+```
+
+### Examples
+
+First, `use` the route namespace:
+
+```PHP
+use framework\route\route;
+```
+
+route is not an object, so you can just make direct operations to the class. Here's the Hello World:
+
+```PHP
+route::get('/', function() {
+  echo 'Hello world!';
+});
+
+route::exec();
+```
+
+route also supports lambda URIs, such as:
+
+```PHP
+route::get('/(:any)', function($slug) {
+  echo 'The slug is: ' . $slug;
+});
+
+route::exec();
+```
+
+You can also make requests for HTTP methods in route, so you could also do:
+
+```PHP
+route::get('/', function() {
+  echo 'I'm a GET request!';
+});
+
+route::post('/', function() {
+  echo 'I'm a POST request!';
+});
+
+route::any('/', function() {
+  echo 'I can be both a GET and a POST request!';
+});
+
+route::exec();
+```
+
+Lastly, if there is no route defined for a certain location, you can make route run a custom callback, like:
+
+```PHP
+route::error(function() {
+  echo '404 :: Not Found';
+});
+```
+
+If you don't specify an error callback, route will just echo `404`.
+
+
+## Example passing to a controller instead of a closure
+<hr>
+It's possible to pass the namespace path to a controller instead of the closure:
+
+For this demo lets say I have a folder called controllers with a demo.php
+
+index.php:
+
+```php
+require('vendor/autoload.php');
+
+use framework\route\route;
+
+route::get('/', 'Controllers\demo@index');
+route::get('page', 'Controllers\demo@page');
+route::get('view/(:num)', 'Controllers\demo@view');
+
+route::exec();
+```
+
+demo.php:
+
+```php
+<?php
+namespace controllers;
+
+class Demo {
+
+    public function index()
+    {
+        echo 'home';
+    }
+
+    public function page()
+    {
+        echo 'page';
+    }
+
+    public function view($id)
+    {
+        echo $id;
+    }
+
+}
+```
+
+This is with route installed via composer.
+
+composer.json:
+
+```
+{
+   "require": {
+        "yzmcms/route": "master"
+    },
+    "autoload": {
+        "psr-4": {
+            "" : ""
+        }
+    }
+}
---
+++ route.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * route路由解析类
+ *
+ * @author           袁志蒙  
+ * @license          http://www.yzmcms.com
+ * @lastmodify       2019-06-30 
+ */
+ 
+namespace framework\route;
+ 
+class route {
+	public static $halts = false;
+	public static $routes = array();
+	public static $methods = array();
+	public static $callbacks = array();
+	public static $maps = array();
+	public static $patterns = array(
+		':any' => '[^/]+',
+		':num' => '[0-9]+',
+		':all' => '.*'
+	);
+	public static $error_callback;
+
+	public static function __callstatic($method, $params) {
+		if ($method == 'map') {
+			$maps = array_map('strtoupper', $params[0]);
+			$uri = strpos($params[1], '/') === 0 ? $params[1] : '/' . $params[1];
+			$callback = $params[2];
+		} else {
+			$maps = null;
+			$uri = strpos($params[0], '/') === 0 ? $params[0] : '/' . $params[0];
+			$callback = $params[1];
+		}
+
+		array_push(self::$maps, $maps);
+		array_push(self::$routes, $uri);
+		array_push(self::$methods, strtoupper($method));
+		array_push(self::$callbacks, $callback);
+	}
+
+	public static function exec(){
+		$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+		$method = $_SERVER['REQUEST_METHOD'];
+
+		$searches = array_keys(static::$patterns);
+		$replaces = array_values(static::$patterns);
+
+		$found_route = false;
+		self::$routes = preg_replace('/\/+/', '/', self::$routes);
+		if (in_array($uri, self::$routes)) {
+			$route_pos = array_keys(self::$routes, $uri);
+			foreach ($route_pos as $route) {
+				if (self::$methods[$route] == $method || self::$methods[$route] == 'ANY' || (!empty(self::$maps[$route]) && in_array($method, self::$maps[$route]))) {
+					$found_route = true;
+					if (!is_object(self::$callbacks[$route])) {
+						$parts = explode('/',self::$callbacks[$route]);
+						$last = end($parts);
+						$segments = explode('@',$last);
+						$controller = new $segments[0]();
+						$controller->{$segments[1]}();
+						if (self::$halts) return;
+					} else {
+						call_user_func(self::$callbacks[$route]);
+						if (self::$halts) return;
+					}
+				}
+			}
+		} else {
+			$pos = 0;
+			foreach (self::$routes as $route) {
+
+				if (strpos($route, ':') !== false) {
+					$route = str_replace($searches, $replaces, $route);
+				}
+				if (preg_match('#^' . $route . '$#', $uri, $matched)) {
+					if (self::$methods[$pos] == $method || self::$methods[$pos] == 'ANY' || (!empty(self::$maps[$pos]) && in_array($method, self::$maps[$pos]))) {
+						$found_route = true;
+						array_shift($matched);
+						if (!is_object(self::$callbacks[$pos])) {
+							$parts = explode('/',self::$callbacks[$pos]);
+							$last = end($parts);
+							$segments = explode('@',$last);
+							$controller = new $segments[0]();
+							if (!method_exists($controller, $segments[1])) {
+								echo "controller and action not found";
+							} else {
+								call_user_func_array(array($controller, $segments[1]), $matched);
+							}
+							if (self::$halts) return;
+						} else {
+							call_user_func_array(self::$callbacks[$pos], $matched);
+							if (self::$halts) return;
+						}
+					}
+				}
+				$pos++;
+			}
+		}
+
+		if ($found_route == false) {
+			if (!self::$error_callback) {
+				self::$error_callback = function() {
+					header($_SERVER['SERVER_PROTOCOL']." 404 Not Found");
+					echo '404';
+				};
+			} else {
+				if (is_string(self::$error_callback)) {
+					self::get($_SERVER['REQUEST_URI'], self::$error_callback);
+					self::$error_callback = null;
+					self::exec();
+					return ;
+				}
+			}
+			call_user_func(self::$error_callback);
+		}
+	}
+}
